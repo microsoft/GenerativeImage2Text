82fd76cadd3837c2ec140b447c1de84bbd1f8511
Joey <sham.joey@gmail.com>
2022-12-12 02:22:53 +0100

    Updated with requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(name='generativeimage2text',
       version='0.1',
       description='Reference implementation of GIT model',
+      install_requires=open("requirements.txt").read().splitlines(),
       packages=find_packages(),
  )
 
